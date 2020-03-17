00ec6dfe6ee73f00f2702e6c6e8d60de8ed99255
HughSuen <62275323+HughSuen@users.noreply.github.com>
2020-03-17 12:52:55 +0700

    test1
    for studing github

---
+++ test.py
@@ -0,0 +1,13 @@
+phone_book = {"John": 123, "Jane": 234, "Jerard": 345}
+print(phone_book["John"])  # 123
+
+bodin = { 'hugh':39561 , "tah":39572 }
+print(bodin['hugh'])
+
+square = 0
+number = 1
+while number < 10:
+    square = number ** 2
+    print(square)
+    number += 1
+    
